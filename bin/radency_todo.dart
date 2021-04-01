a8b56845091cfee40951e04e46186850c32cbfda
StanislawSemenchuk <staniswlavsemenchuk@gmail.com>
2021-04-01 19:00:07 +0300

    first commit

---
+++ bin/radency_todo.dart
@@ -0,0 +1,5 @@
+import 'package:radency_todo/radency_todo.dart' as radency_todo;
+
+void main(List<String> arguments) {
+  print('Hello world: ${radency_todo.calculate()}!');
+}
---
+++ test/radency_todo_test.dart
@@ -0,0 +1,8 @@
+import 'package:radency_todo/radency_todo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
